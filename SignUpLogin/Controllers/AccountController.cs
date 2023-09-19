71cdb6eec6f8860e69aa9820c092557f5e2c6399
iamshivendraa <shivendracpj@gmail.com>
2023-09-19 19:46:19 +0530

    All account view.

---
+++ SignUpLogin/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignUpLogin.Data;
+using SignUpLogin.Models;
+using System.Collections;
+
+namespace SignUpLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Accounts()
+        {
+            List<SignUpModel> accountList = _db.Accounts.ToList();
+            return View(accountList);
+        }
+    }
+}
--- SignUpLogin/Controllers/RegistrationController.cs
+++ SignUpLogin/Controllers/RegistrationController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SignUpLogin.Data;
 using SignUpLogin.Models;
 
 namespace SignUpLogin.Controllers
 {
-
     public class RegistrationController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public RegistrationController(ApplicationDbContext db)
+        public readonly IHttpContextAccessor _context;
+        public RegistrationController(ApplicationDbContext db, IHttpContextAccessor httpContextAccessor)
         {
-
+            _context = httpContextAccessor;
             _db = db;
         }
 
@@ -23,14 +24,19 @@ namespace SignUpLogin.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult SignUp(SignUpModel obj)
         {
-            {
-                if (ModelState.IsValid)
+           
+            if (ModelState.IsValid)
                 {
                     _db.Accounts.Add(obj);
                     _db.SaveChanges();
+                    if (_context.HttpContext.Session.GetString("Email") != null &&
+                        _context.HttpContext.Session.GetString("Email") != "")
+                    {
+                        return RedirectToAction("Accounts","Account");
+                    }
                     return RedirectToAction("Index", "Login");
                 }
-            }
+
             return View(obj);
 
         }
