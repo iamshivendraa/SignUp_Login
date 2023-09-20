2fea5836edddafa102a933fb7821bfb77bb5fd45
iamshivendraa <shivendracpj@gmail.com>
2023-09-20 19:45:07 +0530

    Creating Update Modal.

--- SignUpLogin/Controllers/AccountController.cs
+++ SignUpLogin/Controllers/AccountController.cs
@@ -17,18 +17,34 @@ namespace SignUpLogin.Controllers
         public IActionResult Accounts()
         {
             List<SignUpModel> accountList = _db.Accounts.ToList();
+            
             return View(accountList);
         }
-        
+
+        public IActionResult Update(int? id) 
+        {
+            
+            if(id == null || id==0)
+            {
+                return NotFound();
+            }
+            SignUpModel accountsFromDb = _db.Accounts.Find(id);
+            if(accountsFromDb == null) 
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost]
         public IActionResult Update(SignUpModel obj)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Accounts.Update(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Accounts","Account",obj);
+                return View();
             }
-            return View();
+            return View("Accounts",obj);
         }
     }
 }
