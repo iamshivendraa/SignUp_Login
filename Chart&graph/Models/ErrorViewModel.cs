966e28477f2da89817ec42d3d7a122a4ab16aa3a
iamshivendraa <100219613+iamshivendraa@users.noreply.github.com>
2023-09-26 16:06:51 +0530

    Delete Chart&graph directory

--- Chart&graph/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Chart_graph.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
